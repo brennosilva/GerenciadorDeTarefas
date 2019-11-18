99e2464fa22113a33e046a0ec127cee17750fa41
Brenno <b.renno.silva@hotmail.com>
2019-11-18 15:00:12 -0200

    arquivos do front end

--- GerenciadorDeTarefas.Infraestrutura/DAO/TarefaDAO.cs
+++ GerenciadorDeTarefas.Infraestrutura/DAO/TarefaDAO.cs
@@ -1,9 +1,37 @@
+using System.Collections.Generic;
+using GerenciadorDeTarefas.Dominio.Entidades;
 using GerenciadorDeTarefas.Dominio.Repositorios;
+using NHibernate;
 
 namespace GerenciadorDeTarefas.Infraestrutura.DAO
 {
     public class TarefaDAO : ITarefaRepositorio
     {
-        
+        protected ISession session;
+
+        public TarefaDAO(ISession session)
+        {
+            this.session = session;
+        }
+
+        public void Deletar(Tarefa IdTarefa)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<Tarefa> Listar()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Salvar(Tarefa NovaTarefa)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Ticar(Tarefa IdTarefa)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- GerenciadorDeTarefas.Infraestrutura/DAO/UsuarioDAO.cs
+++ GerenciadorDeTarefas.Infraestrutura/DAO/UsuarioDAO.cs
@@ -1,9 +1,31 @@
+using GerenciadorDeTarefas.Dominio.Entidades;
 using GerenciadorDeTarefas.Dominio.Repositorios;
+using NHibernate;
 
 namespace GerenciadorDeTarefas.Infraestrutura.DAO
 {
     public class UsuarioDAO : IUsuarioRepositorio
     {
-        
+        protected ISession session;
+
+        public UsuarioDAO(ISession session)
+        {
+            this.session = session;
+        }
+
+        public Usuario BuscarPorID(string Id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void MudarPermissao(Usuario Usuario)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Salvar(Usuario NovoUsuario)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
