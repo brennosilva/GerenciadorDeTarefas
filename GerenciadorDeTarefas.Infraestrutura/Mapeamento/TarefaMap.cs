64d1feb594948be8aa0243678df70208babec9d8
Brenno Silva <b.renno.silva@hotmail.com>
2019-11-24 15:49:16 -0300

    configuração para o sql server

--- GerenciadorDeTarefas.Infraestrutura/Mapeamento/TarefaMap.cs
+++ GerenciadorDeTarefas.Infraestrutura/Mapeamento/TarefaMap.cs
@@ -9,7 +9,7 @@ namespace GerenciadorDeTarefas.Infraestrutura.Mapeamento
         public TarefaMap()
         {
             Table("TAREFA");
-            Id(t => t.Id).Column("ID_TAREFA").GeneratedBy.Sequence("SEQ_ID_CATEGORIA");
+            Id(t => t.Id).Column("ID_TAREFA").GeneratedBy.Identity();
             Map(t => t.Nome).Column("NOME");
             Map(t => t.Status).Column("STATUS").CustomType<StatusTarefa>();
 
